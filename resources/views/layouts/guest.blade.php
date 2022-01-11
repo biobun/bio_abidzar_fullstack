afb0a8161dbaca263869b52b0f41a6f8ac3bf488
Bio Abidzar <biobun@gmail.com>
2022-01-11 21:07:56 +0700

    Change login layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,13 +12,14 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
+        @stack('css')
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
+    <body class="bg-white">
+        <div class="container">
             {{ $slot }}
         </div>
+        @stack('script')
     </body>
 </html>
